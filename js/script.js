12473de6bc4f0eee9868940c03efbc0428b92228
vat777 <vitalii.taranchuk@gmail.com>
2023-07-31 17:06:27 +0300

    Add responsive design

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
 
-let nav = document.getElementById('logo-nav');
+let nav = document.getElementById("logo-nav");
 
 function changeBackground () {
     if (window.scrollY > 0) {
-        nav.style.backgroundColor = "black";
+        nav.style.background = "#000";
     } else {
-        nav.style.backgroundColor = 'unset';
-    }
-    
-    console.log(nav.style.backgroundColor);
+        nav.style.background = "";
+    } 
+ 
 }
    
 // changing nav background to black when page scrolled
 window.onscroll = changeBackground; 
 
+let navIcon = document.getElementById("nav-icon");
+let mobileNavContainer = document.getElementById("mobile-nav-container");
 
+function changeNavIcon () {
+    if (navIcon.innerHTML === '<i class="fa-solid fa-x white"></i>') {
+        navIcon.innerHTML = '<i class="fa-solid fa-bars white"></i>';
+        mobileNavContainer.style.opacity = 0;
+        mobileNavContainer.style.top = "-150px";
+        navIcon.style.transform = "rotate(0deg)";
+    } else {
+        
+
+        navIcon.innerHTML = '<i class="fa-solid fa-x white"></i>';
+        mobileNavContainer.style.top = "80px";
+        mobileNavContainer.style.opacity = 1;
+        navIcon.style.transform = "rotate(-180deg)";
+    }
+    
+    
+}
+
+navIcon.onclick = changeNavIcon;
+// mobileNavContainer.onmouseout = changeNavIcon;
